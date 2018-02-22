a9c7e80b06e8b61d3ff2f4e99db546afbb18b4fd
Elanas <elaasinfo@gmail.com>
2018-02-22 15:53:09 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/vcs.xml

--- src/main/java/lt/findxcheap/pirmas/controller/ComparisonController.java
+++ src/main/java/lt/findxcheap/pirmas/controller/ComparisonController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller // <-- būtina
-public class IndexController {
-    @RequestMapping("/")
+@Controller
+public class ComparisonController {
+
+    @RequestMapping("/comparison")
     String index(
             @RequestParam(
                     value = "search",
@@ -19,7 +19,14 @@ public class IndexController {
             Model model
     ) {
         model.addAttribute("search", search);
-        return "index";
+
+
+
+
+
+        return "comparison";
 
     }
 }
+
+
